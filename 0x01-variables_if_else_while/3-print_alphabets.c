7d14f156b9428952cb517f3166dd1e4bb97c97c5
Janine <janinebekker94@gmail.com>
2023-04-24 10:05:23 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,13 @@ int main(void)
 	int n = 97;
 	int m = 65;
 
-	while (m <= 90)
-	{
-		putchar(m);
-	}
 	while (n <= 122)
 	{
 		putchar(n);
+	}
+	while (m <= 90)
+	{
+		putchar(m);
 	}
 	putchar('\n');
 	return (0);
