93f460e460cde827b2fab325efee3e7b02100a62
Janine <janinebekker94@gmail.com>
2023-04-24 09:45:40 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Main entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (m <= 90)
+	{
+		putchar(m);
+	}
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	putchar('/n');
+
+	return (0);
+}
