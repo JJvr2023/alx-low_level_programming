88fb227808082cc176cf9f500e3919b86fa1f94a
Janine <janinebekker94@gmail.com>
2023-04-24 10:28:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Main entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+			n++;
+		{
+			continue;
+		}
+		putchar(n);
+	}
+	return (0);
+}
