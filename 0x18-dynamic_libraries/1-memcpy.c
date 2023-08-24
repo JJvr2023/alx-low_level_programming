7896353dfc2026605bbc076fb5ca3d967c8a0390
JJvr2023 <129042030+JJvr2023@users.noreply.github.com>
2023-08-24 12:18:37 +0200

    Update 1-memcpy.c

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "main.h"
 
-void memcpy(char *dest, const char *src, int n) {
+void memcpy(char *dest, const char *src, size_t n) {
   int i;
 
   for (i = 0; i < n; i++) {
