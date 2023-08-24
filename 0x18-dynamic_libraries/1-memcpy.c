93452d66107167d72510a290c60548cacd2fc21d
JJvr2023 <129042030+JJvr2023@users.noreply.github.com>
2023-08-24 12:15:17 +0200

    Update 1-memcpy.c

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -2,18 +2,17 @@
 #include <string.h>
 #include "main.h"
 
-void *memcpy(void *dest, const void *src, size_t n) {
-  unsigned char *d = (unsigned char *)dest;
-  unsigned char *s = (unsigned char *)src;
-  while (n--) {
-    *d++ = *s++;
+void memcpy(char *dest, const char *src, int n) {
+  int i;
+
+  for (i = 0; i < n; i++) {
+    dest[i] = src[i];
   }
-  return dest;
 }
 
 int main() {
   char dest[10];
-  char src[10] = "This is a string.";
+  char src[] = "This is a string.";
 
   memcpy(dest, src, sizeof(src));
 
