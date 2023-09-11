259030e3cdcd371b583c4eb460cac08ecb1a036f
JJvr2023 <129042030+JJvr2023@users.noreply.github.com>
2023-09-11 08:41:37 +0200

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,12 @@
-#include &quot;hash_tables.h&quot;
-/**
-* hash_djb2 - function to compute hash of &#39;str&#39; using djb2
-* @str: string to hash
-*
-* Return: value of 64 bits hash
-*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash;
-int c;
-hash = 5381;
+    unsigned long int hash;
+    int c;
 
-while ((c = *str++))
-{
-hash = ((hash &lt;&lt; 5) + hash) + c; /* hash * 33 + c */
-}
-return (hash);
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
 }
